faabc799e2fb786775b9c76baa9f7920052f83ae
Itim-B <58975660+Itim-B@users.noreply.github.com>
2021-03-08 17:17:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import soft_information
+import time
+pas_temps=1
 
-SI_list = soft_information.parse_scenario('soft_information/data/si1.json')
-positions, _ = soft_information.compute_positions(SI_list, nb_points=3)
+listepos=[]
+while(1):
+    SI_list = soft_information.parse_scenario('soft_information/data/si1_2.json')
+    positions, _ = soft_information.compute_positions(SI_list, 2, listepos)
+    print(positions.round(3))
+    listepos+=[positions]
+    time.sleep(pas_temps)
 
-print(positions.round())
